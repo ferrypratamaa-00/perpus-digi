e6c431f300bb717aeb786be1fee94b04f8796d25
Ferry Agus Pratama <ferryaguspratama@gmail.com>
2025-04-11 23:01:07 +0700

    feat(server) : update slug become more unique by adding a random string at the end of slug

--- server/src/shared/utils/slug.util.ts
+++ server/src/shared/utils/slug.util.ts
@@ -1,6 +1,19 @@
+import { randomBytes } from "crypto";
+
 export const generateSlug = (text: string) => {
-    return text
+    const baseSlug = text
         .toLowerCase()
         .replace(/[^a-z0-9]+/g, "-")
         .replace(/(^-|-$)+/g, "");
+
+    const suffix = generateSecureRandomString();
+    return `${baseSlug}-${suffix}`;
 };
+
+function generateSecureRandomString(length = 5): string {
+    const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const bytes = randomBytes(length);
+    return Array.from(bytes)
+        .map((b) => chars[b % chars.length])
+        .join("");
+}
