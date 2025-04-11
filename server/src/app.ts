50fa21b7bbd641e75d9294f635fe898983e34556
Ferry Agus Pratama <ferryaguspratama@gmail.com>
2025-04-11 16:07:37 +0700

    feat(server): add global error handler and fallback error catcher

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,16 @@
 import { Hono } from "hono";
 import { routes } from "@/api/v1/index.route";
+import { notFoundHandler } from "./shared/middlewares/notFound.middleware";
+import {
+    errorHandler,
+    onErrorHandler,
+} from "./shared/middlewares/error.middlleware";
 
 export const app = new Hono();
 
 app.route("/api/v1", routes);
+
+app.notFound(notFoundHandler);
+
+app.use("*", errorHandler);
+app.onError(onErrorHandler);
--- server/src/shared/middlewares/error.middlleware.ts
+++ server/src/shared/middlewares/error.middlleware.ts
@@ -1,5 +1,5 @@
-import { AppError } from "@/core/errors/appError.error";
 import { Context } from "hono";
+import { AppError } from "@/core/errors/appError.error";
 import { handleError } from "../utils/response.util";
 
 export const errorHandler = async (c: Context, next: () => Promise<void>) => {
@@ -10,6 +10,14 @@ export const errorHandler = async (c: Context, next: () => Promise<void>) => {
         const message = err.message || "Internal Server Error";
         const errors = err.errors || null;
 
-        handleError(c, message, status, errors);
+        return handleError(c, message, status, errors);
     }
+};
+
+export const onErrorHandler = (err: Error, c: Context) => {
+    const status = err instanceof AppError ? err.statusCode : 500;
+    const message = err.message || "Internal Server Error";
+    const errors = (err as any).errors || null;
+
+    return handleError(c, message, status, errors);
 };
