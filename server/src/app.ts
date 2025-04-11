617c6359d3d4dd5ba7481d296f210edea05107a6
Ferry Agus Pratama <ferryaguspratama@gmail.com>
2025-04-11 22:59:09 +0700

    feat(server): implementation cors logger and handling header security

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 import { routes } from "@/api/v1/index.route";
 import { notFoundHandler } from "./shared/middlewares/notFound.middleware";
 import {
@@ -8,6 +10,13 @@ import {
 
 export const app = new Hono();
 
+app.use("/api/*", cors());
+app.use("*", logger());
+app.use("*", async (c, next) => {
+    c.res.headers.set("X-Content-Type-Options", "nosniff");
+    c.res.headers.set("X-Frame-Options", "DENY");
+    await next();
+});
 app.route("/api/v1", routes);
 
 app.notFound(notFoundHandler);
