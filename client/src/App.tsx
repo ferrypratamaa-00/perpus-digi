16c24cf81c8fc2b7c9098357fe10fd0ef43e0277
Ferry Agus Pratama <ferryaguspratama@gmail.com>
2025-04-12 05:47:37 +0700

    feat(client): setup react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,30 @@
+import { Outlet } from "react-router";
+
 function App() {
     return (
         <>
             <div className="h1">is coming..</div>
+            <Outlet />
         </>
     );
 }
 
 export default App;
+
+function Home() {
+    return (
+        <>
+            <div className="h1">Home..</div>
+        </>
+    );
+}
+
+function About() {
+    return (
+        <>
+            <div className="h1">About..</div>
+        </>
+    );
+}
+
+export { Home, About };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App, { About, Home } from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: App,
+        children: [
+            { index: true, Component: Home },
+            { path: "about", Component: About },
+        ],
+    },
+]);
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
+        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}></QueryClientProvider>
     </StrictMode>
 );
